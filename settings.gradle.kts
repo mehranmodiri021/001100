455f73d3bec6ae0ff934cafc8b593c7bb8e5309c
mehranmodiri021 <mehranmodiri021@gmail.com>
2026-09-20 15:33:03 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,31 @@
 pluginManagement {
-  repositories {
-    google {
-      content {
-        includeGroupByRegex("com\\.android.*")
-        includeGroupByRegex("com\\.google.*")
-        includeGroupByRegex("androidx.*")
-      }
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
     }
-    mavenCentral()
-    gradlePluginPortal()
-  }
 }
 
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-    maven { url = java.net.URI.create("https://repo.tapsell.ir/repository/tapsell-android/") }
-  }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // مخزن اختصاصی تپسل برای دانلود SDK تپسل پلاس
+        maven { url = uri("https://repo.tapsell.ir/repository/tapsell-android/") }
+        // مخزن JitPack برای Poolakey (IAP کافه‌بازار)
+        maven { url = uri("https://jitpack.io") }
+    }
 }
 
 rootProject.name = "Arena Clash"
