7cf651b7782bd7c4f75eb3eed9367f28e0cbd28f
mehranmodiri021 <mehranmodiri021@gmail.com>
2026-09-20 17:20:51 +0330

    Update Screen.kt

--- app/src/main/java/com/example/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/ui/navigation/Screen.kt
@@ -1,4 +1,4 @@
-package com.example.ui.navigation
+package com.arenaclash.game.ui.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.EmojiEvents
