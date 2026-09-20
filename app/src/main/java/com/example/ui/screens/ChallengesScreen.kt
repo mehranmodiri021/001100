e1378eb25f4237e32f69cdc3c4abc2672f8c54d8
mehranmodiri021 <mehranmodiri021@gmail.com>
2026-09-20 17:10:21 +0330

    Update ChallengesScreen.kt

--- app/src/main/java/com/example/ui/screens/ChallengesScreen.kt
+++ app/src/main/java/com/example/ui/screens/ChallengesScreen.kt
@@ -1,4 +1,4 @@
-package com.example.ui.screens
+package com.arenaclash.game.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -39,7 +39,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.example.ui.viewmodel.ArenaViewModel
+import com.arenaclash.game.ui.viewmodel.ArenaViewModel
 
 @Composable
 fun ChallengesScreen(
