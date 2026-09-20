7999597a1d55a89b550ad902791b7fbe5eee9285
mehranmodiri021 <mehranmodiri021@gmail.com>
2026-09-20 17:11:28 +0330

    Update LeaderboardScreen.kt

--- app/src/main/java/com/example/ui/screens/LeaderboardScreen.kt
+++ app/src/main/java/com/example/ui/screens/LeaderboardScreen.kt
@@ -1,4 +1,4 @@
-package com.example.ui.screens
+package com.arenaclash.game.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -36,7 +36,7 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.ui.viewmodel.ArenaViewModel
+import com.arenaclash.game.ui.viewmodel.ArenaViewModel
 
 @Composable
 fun LeaderboardScreen(
